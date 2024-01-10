8220bc1d8e98c86352e27a97a9cc4f905de45c8b
valentin.gibbe <valentin.gibbe@epita.fr>
2024-01-10 19:07:15 +0100

    fix<parser>: fix memory leaks

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,5 +1,19 @@
 #include "parser.h"
 
+enum ast_type parser_peek(struct lexer *lexer)
+{
+    struct token tok = lexer_peek(lexer);
+    free(tok.data);
+    return tok.type;
+}
+
+enum ast_type parser_pop(struct lexer *lexer)
+{
+    struct token tok = lexer_pop(lexer);
+    free(tok.data);
+    return tok.type;
+}
+
 struct ast_node *parse(struct lexer *lexer)
 {
     return input(lexer);
