54f131fe4447f42cb7901c212c427cac15775025
erwann lesech <erwann.lesech@epita.fr>
2024-01-11 02:11:31 +0100

    build<main>: fix Makefiles to gen binary lexer/parser/io
    This commit should fix makefiles to generate the binary with all includes of lexer, parser, ast and io. Plus it allows to run testsuite of lexer and io.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,22 @@
 #include "io_backend/io_backend.h"
+#include "parser/parser.h"
+#include "ast/ast.h"
+#include "lexer/lexer.h"
 
 int main(int argc, char **argv)
 {
-	printf("%s\n", io_backend(argc, argv));
+	char *input = io_backend(argc, argv);
+
+	printf("%s\n", input);
+	
+	struct lexer *lexer = lexer_new(input);
+
+	struct ast_node *ast = parse(lexer);
+
+	print_ast(ast, 0);
+
+	ast_free(ast);
+	lexer_free(lexer);
+	free(input);
 	return 0;
 }
